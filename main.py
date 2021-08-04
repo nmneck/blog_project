9253236030ad397f424bed637d80da7cefa75841
nmneck <nmneck23@gmail.com>
2021-08-04 12:56:34 +0530

    disable few code blocks

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=Fa
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
 
 
 ##CONFIGURE TABLE
@@ -76,14 +76,14 @@ class Images(db.Model):
 db.create_all()
 
 
-def admin_only(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if current_user.id != 1:
-            return abort(403)
-        return f(*args, **kwargs)
-
-    return decorated_function
+# def admin_only(f):
+#     @wraps(f)
+#     def decorated_function(*args, **kwargs):
+#         if current_user.id != 1:
+#             return abort(403)
+#         return f(*args, **kwargs)
+#
+#     return decorated_function
 
 
 @app.route('/')
@@ -104,7 +104,7 @@ def upload():
         )
         db.session.add(new_image)
         db.session.commit()
-    return render_template("upload.html", form=form)
+    return render_template("upload.html", form=form, current_user=current_user)
 
 
 @app.route('/images/<category>')
@@ -176,92 +176,92 @@ def login():
     return render_template("login.html", form=form, current_user=current_user)
 
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('get_all_posts'))
+# @app.route('/logout')
+# def logout():
+#     logout_user()
+#     return redirect(url_for('get_all_posts'))
 
 
-@app.route("/post/<int:post_id>", methods=["GET", "POST"])
-def show_post(post_id):
-    form = CommentForm()
-    requested_post = BlogPost.query.get(post_id)
+# @app.route("/post/<int:post_id>", methods=["GET", "POST"])
+# def show_post(post_id):
+#     form = CommentForm()
+#     requested_post = BlogPost.query.get(post_id)
+#
+#     if form.validate_on_submit():
+#         if not current_user.is_authenticated:
+#             flash("You need to login or register to comment.")
+#             return redirect(url_for("login"))
+#
+#         new_comment = Comment(
+#             text=form.comment_text.data,
+#             comment_author=current_user,
+#             parent_post=requested_post
+#         )
+#         db.session.add(new_comment)
+#         db.session.commit()
+#
+#     return render_template("post.html", post=requested_post, form=form, current_user=current_user)
 
-    if form.validate_on_submit():
-        if not current_user.is_authenticated:
-            flash("You need to login or register to comment.")
-            return redirect(url_for("login"))
-
-        new_comment = Comment(
-            text=form.comment_text.data,
-            comment_author=current_user,
-            parent_post=requested_post
-        )
-        db.session.add(new_comment)
-        db.session.commit()
-
-    return render_template("post.html", post=requested_post, form=form, current_user=current_user)
-
-
-@app.route("/about")
-def about():
-    return render_template("about.html", current_user=current_user)
-
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html", current_user=current_user)
-
-
-@app.route("/new-post", methods=["GET", "POST"])
-@admin_only
-def add_new_post():
-    form = CreatePostForm()
-    if form.validate_on_submit():
-        new_post = BlogPost(
-            title=form.title.data,
-            subtitle=form.subtitle.data,
-            body=form.body.data,
-            img_url=form.img_url.data,
-            author=current_user,
-            date=date.today().strftime("%B %d, %Y")
-        )
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect(url_for("get_all_posts"))
-
-    return render_template("make-post.html", form=form, current_user=current_user)
-
-
-@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
-@admin_only
-def edit_post(post_id):
-    post = BlogPost.query.get(post_id)
-    edit_form = CreatePostForm(
-        title=post.title,
-        subtitle=post.subtitle,
-        img_url=post.img_url,
-        author=current_user,
-        body=post.body
-    )
-    if edit_form.validate_on_submit():
-        post.title = edit_form.title.data
-        post.subtitle = edit_form.subtitle.data
-        post.img_url = edit_form.img_url.data
-        post.body = edit_form.body.data
-        db.session.commit()
-        return redirect(url_for("show_post", post_id=post.id))
-
-    return render_template("make-post.html", form=edit_form, is_edit=True, current_user=current_user)
+#
+# @app.route("/about")
+# def about():
+#     return render_template("about.html", current_user=current_user)
+#
+#
+# @app.route("/contact")
+# def contact():
+#     return render_template("contact.html", current_user=current_user)
+#
+#
+# @app.route("/new-post", methods=["GET", "POST"])
+# @admin_only
+# def add_new_post():
+#     form = CreatePostForm()
+#     if form.validate_on_submit():
+#         new_post = BlogPost(
+#             title=form.title.data,
+#             subtitle=form.subtitle.data,
+#             body=form.body.data,
+#             img_url=form.img_url.data,
+#             author=current_user,
+#             date=date.today().strftime("%B %d, %Y")
+#         )
+#         db.session.add(new_post)
+#         db.session.commit()
+#         return redirect(url_for("get_all_posts"))
+#
+#     return render_template("make-post.html", form=form, current_user=current_user)
 
 
-@app.route("/delete/<int:post_id>")
-@admin_only
-def delete_post(post_id):
-    post_to_delete = BlogPost.query.get(post_id)
-    db.session.delete(post_to_delete)
-    db.session.commit()
-    return redirect(url_for('get_all_posts'))
+# @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+# @admin_only
+# def edit_post(post_id):
+#     post = BlogPost.query.get(post_id)
+#     edit_form = CreatePostForm(
+#         title=post.title,
+#         subtitle=post.subtitle,
+#         img_url=post.img_url,
+#         author=current_user,
+#         body=post.body
+#     )
+#     if edit_form.validate_on_submit():
+#         post.title = edit_form.title.data
+#         post.subtitle = edit_form.subtitle.data
+#         post.img_url = edit_form.img_url.data
+#         post.body = edit_form.body.data
+#         db.session.commit()
+#         return redirect(url_for("show_post", post_id=post.id))
+#
+#     return render_template("make-post.html", form=edit_form, is_edit=True, current_user=current_user)
+
+#
+# @app.route("/delete/<int:post_id>")
+# @admin_only
+# def delete_post(post_id):
+#     post_to_delete = BlogPost.query.get(post_id)
+#     db.session.delete(post_to_delete)
+#     db.session.commit()
+#     return redirect(url_for('get_all_posts'))
 
 
 if __name__ == "__main__":
