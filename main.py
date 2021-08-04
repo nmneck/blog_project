95ee33eca62be640526e2c03086fc2ea053649a3
nmneck <nmneck23@gmail.com>
2021-08-04 13:05:11 +0530

    Added images to table

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ class User(UserMixin, db.Model):
     name = db.Column(db.String(100))
     posts = relationship("BlogPost", back_populates="author")
     comments = relationship("Comment", back_populates="comment_author")
+    images = relationship("Images")
 
 
 class BlogPost(db.Model):
